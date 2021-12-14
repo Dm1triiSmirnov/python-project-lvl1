8e9cabd636aec393be518fd58fc17b0887ec8aa9
Dm1triiSmirnov <smirnov0719@gmail.com>
2021-12-14 17:38:03 +0300

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,5 +7,5 @@ task = 'Find the greatest common divisor of given numbers.'
 def get_game_data():
     x, y = randint(1, 100), randint(1, 100)
     question = f'{x} {y}'
-    correct_answer = gcd(x, y)
+    correct_answer = str(gcd(x, y))
     return question, correct_answer
