04241f6bd15527fcae66398758e40480566604c1
Dm1triiSmirnov <smirnov0719@gmail.com>
2021-12-15 18:57:30 +0300

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,12 +4,12 @@ task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_game_data():
-    question = randint(1, 100)
-    if question == 1:
-        correct_answer = 'no'
+    question = randint(2, 100)
+    if question == 2:
+        correct_answer = 'yes'
     else:
-        for i in range(1, question):
-            if question % i == 0 and i != 1:
+        for i in range(2, question):
+            if question % i == 0:
                 correct_answer = 'no'
                 break
             else:
