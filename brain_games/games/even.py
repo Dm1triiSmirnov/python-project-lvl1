1c724956e3900d3e289db4e693e7d6ed7e66548a
Dm1triiSmirnov <smirnov0719@gmail.com>
2021-12-16 18:46:21 +0300

    fix brain_even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,4 +7,5 @@ TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 def get_game_data():
     num = randrange(1, 101)
     correct_answer = ('no', 'yes')[num % 2 == 0]
+    num = str(num)
     return num, correct_answer
