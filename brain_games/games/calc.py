0ca31e993636d9860b044d986a2b5ddb3daebead
Dm1triiSmirnov <smirnov0719@gmail.com>
2021-12-16 19:22:42 +0300

    fix errors as per hexlet-check

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,6 +6,6 @@ TASK = 'What is the result of the expression?'
 
 def get_game_data():
     symbol = choice(['+', '-', '*'])
-    question = str(randrange(1, 101)) + symbol + str(randrange(1, 101))
+    question = str(randrange(1, 101)) + ' ' + symbol + ' ' + str(randrange(1, 101))
     correct_answer = str(eval(question))
     return question, correct_answer
