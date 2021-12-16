adda3e22ec1f71c091322954a3deaa024d74eb38
Dm1triiSmirnov <smirnov0719@gmail.com>
2021-12-16 18:59:20 +0300

    fix errors as per hexlet-check

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,5 +7,5 @@ TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 def get_game_data():
     num = randrange(1, 101)
     correct_answer = ('no', 'yes')[num % 2 == 0]
-    question = str(num)
+    question = num
     return question, correct_answer
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,5 +1,5 @@
 from brain_games.engine import start
-from brain_games.games.calc import calc
+from brain_games.games import calc
 
 
 def main():
