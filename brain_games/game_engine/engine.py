8fa93bf7fbf9972efcefb5bdf9408a86ffdb07b1
Dm1triiSmirnov <smirnov0719@gmail.com>
2021-12-18 14:42:51 +0300

    fix errors as per CodeClimate issues

--- brain_games/game_engine/engine.py
+++ brain_games/game_engine/engine.py
@@ -4,9 +4,8 @@ GREETINGS = 'Welcome to the Brain Games!'
 NAME_REQUEST = 'May I have your name? '
 SAY_HELLO = 'Hello, {}!'
 WIN_SCORE = 3
-STARTING_SCORE = 0
 WIN_MESSAGE = 'Congratulations, {}!'
-QUESTION = 'Question: {}'
+QUESTION_MSG = 'Question: {}'
 USER_ANSWER = 'Your answer: {}'
 CORRECT = 'Correct!'
 WRONG_ANSWER = "'{}' is wrong answer ;(. " "Correct answer was '{}'." \
@@ -18,19 +17,19 @@ def start(game):
     name = prompt.string(NAME_REQUEST)
     print(SAY_HELLO.format(name))
     print(game.TASK)
-    score = STARTING_SCORE
+    scores = 0
 
     while True:
-        if score == WIN_SCORE:
+        if scores == WIN_SCORE:
             result = WIN_MESSAGE.format(name)
             break
         else:
             question, correct_answer = game.get_game_data()
-            print(QUESTION.format(question))
+            print(QUESTION_MSG.format(question))
             user_answer = input()
             print(USER_ANSWER.format(user_answer))
             if user_answer == correct_answer:
-                score += 1
+                scores += 1
                 print(CORRECT)
             else:
                 result = WRONG_ANSWER.format(user_answer, correct_answer, name)
